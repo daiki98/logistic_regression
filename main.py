4475b288c43eabec269b91270bc30cef7d3f05a7
daiki98 <zdymori@gmail.com>
2022-01-01 15:57:25 -0800

    implemented logistic regression except visualization

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import numpy as np
 
-z = b0 + b1 * X1 + b2 * X2
-sigmoid = 1 / (1 + np.exp(-z))
+
+input_data = np.array([1, 1, 0], [1, 4, 0], [3, 1, 0], [4, 5, 1], [0, 7, 1])
+data_number = input_data.shape[0]
 
 
+epochs = 100
+alpha = 0.1
 
-if Y == 1:
-    db0 = db0 + (1 - sigmoid)
-    db1 = db1 + (1 - sigmoid) * X1
-    db2 = db2 + (1 - sigmoid) * X2
-else:
-    db0 = db0 - sigmoid
-    db1 = db1 - sigmoid * X1
-    db2 = db2 - sigmoid * X2
+b0 = 0.1
+b1 = 0.1
+b2 = 0.1
+
+for t in range(epochs):
+    db0 = 0
+    db1 = 0
+    db2 = 0
+    for i in range(data_number):
+        z = b0 + b1 * input_data[i, 0] + b2 * input_data[i, 1]
+        sigmoid = 1 / (1 + np.exp(-z))
+
+        if input_data[i, 2] == 1:
+            db0 = db0 + (1 - sigmoid)
+            db1 = db1 + (1 - sigmoid) * input_data[i, 0]
+            db2 = db2 + (1 - sigmoid) * input_data[i, 1]
+        else:
+            db0 = db0 - sigmoid
+            db1 = db1 - sigmoid * input_data[i, 0]
+            db2 = db2 - sigmoid * input_data[i, 1]
 
 b0 = b0 + alpha * db0
 b1 = b1 + alpha * db1
